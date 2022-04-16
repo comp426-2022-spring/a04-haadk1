6a08c0acc28cbbf1c27fd96626444f261ecc20d1
Haad Khan <haadk@live.unc.edu>
2022-04-16 00:46:04 -0400

    try6

--- server.js
+++ server.js
@@ -9,14 +9,25 @@ const fs = require('fs')
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-arguments["port"]
 
-
-var port = arguments.port || process.env.port || 5000
+const port = arguments.port || process.env.port || 5000
 
 const server = app.listen(port, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
+
+
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+    res.statusCode = 200;
+    // Respond with status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+    res.end(res.statusCode + ' ' + res.statusMessage)
+});
+
+
+
 
 if (arguments.log == 'false'){
     console.log("not creating access.log")
@@ -85,17 +96,40 @@ if (arguments.debug || arguments.d){
     })
 }
 
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-    res.statusCode = 200;
-    // Respond with status message "OK"
-    res.statusMessage = 'OK';
-    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
-    res.end(res.statusCode + ' ' + res.statusMessage)
-});
 
 
 
+app.get('/app/flip/', (req, res) => {
+    var flip = coinFlip()
+    res.status(200).json({ 'flip' : flip })
+ })  
+ 
+ 
+ app.get('/app/flips/:number', (req, res) => {
+     const finalFlips = coinFlips(req.params.number)
+     res.status(200).json({ 'raw': finalFlips, 'summary': countFlips(finalFlips) })
+ 
+ }
+ )
+ 
+ app.get('/app/flip/call/heads', (req, res) => {
+     const flipRandomCoin = flipACoin("heads")
+     res.status(200).json( {"call": flipRandomCoin.call, "flip": flipRandomCoin.flip, "result": flipRandomCoin.result})
+ })
+ 
+ app.get('/app/flip/call/tails', (req, res) => {
+     const flipRandomCoin = flipACoin("tails")
+     res.status(200).json( {"call": flipRandomCoin.call, "flip": flipRandomCoin.flip, "result": flipRandomCoin.result})
+ })
+ 
+ 
+ 
+ // Default response for any other request
+ app.use(function(req, res){
+     res.status(404).send('404 NOT FOUND')
+ 
+ })
+ 
 
 
 
